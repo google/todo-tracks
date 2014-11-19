a5d4ea71798dc59813f6aa33bcdbf341703cc2b9
Wei Zheng <weizheng@google.com>
2014-11-18 16:59:21 -0800

    use bootstrap rows to list branches

--- src/ui/todo_tracker.js
+++ src/ui/todo_tracker.js
@@ -6,12 +6,12 @@ todoTrackerApp.controller("listBranches", function($scope) {
   $scope.repositories = [
     {repo_name:'local',
       branches:[
-        {branch_name:'master',info:'master_info'},
-        {branch_name:'weizheng-dev',info:'weizheng-dev_info'},
-        {branch_name:'ojarjur-dev',info:'ojarjur-dev_info'}]},
+        {branch_name:'master',last_modified:'Nov 10 by Wei',link:'xx'},
+        {branch_name:'weizheng-dev',last_modified:'Nov 10 by Wei',link:'xx'},
+        {branch_name:'ojarjur-dev',last_modified:'Nov 10 by Wei',link:'xx'}]},
     {repo_name:'remote',
       branches:[
-        {branch_name:'master_on_remote',info:'master_on_remote_info'},
-        {branch_name:'weizheng-dev_on_remote',info:'weizheng-dev_on_remote_info'},
-        {branch_name:'ojarjur-dev_on_remote',info:'ojarjur-dev_on_remote_info'}]}];
+        {branch_name:'master_on_remote',last_modified:'Nov 10 by Wei',link:'xx'},
+        {branch_name:'weizheng-dev_on_remote',last_modified:'Nov 10 by Wei',link:'xx'},
+        {branch_name:'ojarjur-dev_on_remote',last_modified:'Nov 10 by Wei',link:'xx'}]}];
 });
