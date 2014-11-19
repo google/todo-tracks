f5f726cf6134eb3938652997d82fb5440aea681a
Wei Zheng <weizheng@google.com>
2014-11-19 13:33:22 -0800

    list branch works with reading JSON from HTTP request

--- src/ui/todo_tracker.js
+++ src/ui/todo_tracker.js
@@ -3,9 +3,6 @@
  */
 var todoTrackerApp=angular.module("todoTrackerApp", []);
 todoTrackerApp.controller("listBranches", function($scope,$http) {
-  alert("hello, controller");
-  $http.get("zz_list_branches_json.html")
-    .success(function(response) {$scope.repositories = response;})
-    .error(function(data,status,headers,config) {
-    alert("failed: data" + data + ", status: " + status};
+  $http.get(window.location.protocol + "//" + window.location.host + "/zz_list_branches_json.html")
+    .success(function(response) {$scope.repositories = response;});
 });
